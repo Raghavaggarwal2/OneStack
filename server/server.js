83678d2b6bf272cdb19ffc980a545a3cabf897e5
Laksh Nijhawan <86841869+laksh2005@users.noreply.github.com>
2025-05-09 20:15:51 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -17,7 +17,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Routes
 app.use('/api/auth', authRoutes);
